dd78770de666db767dbc9078b60a78b8fbb4ad4c
phiibeee <sophiatapiaig88@gmail.com>
2026-07-01 15:45:37 -0400

    Add files via upload

--- src/JugadorEstudiante.h
+++ src/JugadorEstudiante.h
@@ -14,7 +14,7 @@ class JugadorEstudiante : public Jugador{
         std::string nombreEstudiante;
 
     public:
-        JugadorEstudiante() {
+        JugadorEstudiante(std::string nombre = "JugadorEstudiante") : Jugador(nombre) {
             nombreEstudiante = "Sophia Ignacia Gulppi Tapia"; 
         }
 
@@ -24,6 +24,9 @@ class JugadorEstudiante : public Jugador{
             const std::vector<int> &dados,
             const Anotacion &resultadoPrevio) override {
 
+            //obtener el marcador (tablero)
+            const Marcador& miMarcador = marcadores.at(this->nombre);
+            
             //ver si obtuvo la dormida (gana inmediatamente)  
             for(size_t i = 0; i < actuacionesPosibles.size(); i++){
                 if(actuacionesPosibles[i].accion == "dormida"){
@@ -32,32 +35,55 @@ class JugadorEstudiante : public Jugador{
             }
             for(size_t i = 0; i < actuacionesPosibles.size(); i++){
                 if(actuacionesPosibles[i].accion == "anotar"){
-                    //fin de turno, momento de decidir que y donde anotar el puntaje
-                    if(actuacionesPosibles[i].anotacion.juego == "full" && actuacionesPosibles[i].anotacion.puntos > 0||
-                    actuacionesPosibles[i].anotacion.juego == "poker" && actuacionesPosibles[i].anotacion.puntos > 0 ||
-                    actuacionesPosibles[i].anotacion.juego == "escalera" && actuacionesPosibles[i].anotacion.puntos > 0){
-                        return i;
+                    std::string juego = actuacionesPosibles[i].anotacion.juego;
+                    int puntaje = actuacionesPosibles[i].anotacion.puntos;
+                    if(miMarcador.yaAnotado(juego) == false){
+                        if(lanzamiento == 0){
+                            if(juego == "grande" || (juego == "poker" && puntaje >= 45)|| (juego == "full" && puntaje >= 35) || (juego == "escalera" && puntaje >= 25)){
+                                return i;
+                            }
+                        }else{
+                            if(juego == "grande" || (juego == "poker" && puntaje >= 40) || (juego == "full" && puntaje >= 30) || (juego == "escalera" && puntaje >= 20)){
+                                return i;
+                            }
+                        }
                     }
                 }
             }
             //si es que aun nos quedan lanzamientos en el turno, relanzar
             if(lanzamiento < 2){
+                int relanzar = -1;
+                int dados = -1;
                 for(size_t i = 0; i < actuacionesPosibles.size(); i++){
-                    if(actuacionesPosibles[i].accion == "relanzar"){
-                        return i;
+                    if(actuacionesPosibles[i].accion == "lanzar"){
+                        int cant_dados = actuacionesPosibles[i].indiceDados.size();
+                        if(cant_dados > dados){
+                            dados = cant_dados;
+                            relanzar = i;
+                        }
                     }
                 }
+                if(relanzar != -1) return relanzar;
             }
             //si es que no quedan mas lanzamientos anotar el mayor puntaje
             int puntaje_max = -1; //
-            int index = 0;
+            int index = -1;
             for(size_t i = 0; i < actuacionesPosibles.size(); i++){
                 if(actuacionesPosibles[i].accion == "anotar"){
-                    if(actuacionesPosibles[i].anotacion.puntos > puntaje_max){
-                        puntaje_max = actuacionesPosibles[i].anotacion.puntos;
-                        index = i;
+                    std::string juego = actuacionesPosibles[i].anotacion.juego;
+                    int puntos = actuacionesPosibles[i].anotacion.puntos;
+                    if(miMarcador.yaAnotado(juego) == false){
+                        if(puntos > puntaje_max){
+                            puntaje_max = puntos;
+                            index = i;
+                        }
                     }
                 }
+            }
+            if (index != -1) {
+                return index;
+            } else {
+                return 0;
             }
         }
         std::string getNombreEstudiante() const {
